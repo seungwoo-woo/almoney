551cb2f02ac96ee8ba3f0bf771107243466b82e0
seungwoo-woo <seungwoo.woo@gmail.com>
2023-11-19 18:09:04 +0900

    gitignore 삭제

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7bJu2x2b-Phzy-ZVBwsdzC7zLROusJxQ",
+  authDomain: "gotowork-d1752.firebaseapp.com",
+  projectId: "gotowork-d1752",
+  storageBucket: "gotowork-d1752.appspot.com",
+  messagingSenderId: "240198868072",
+  appId: "1:240198868072:web:007f144a81e432286f6fdd",
+  measurementId: "G-DC2HZMEN0M"
+};
+
+export { firebaseConfig };
